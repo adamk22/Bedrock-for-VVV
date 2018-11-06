bb091ba15eadd156b9ae98783b1ba5b635512076
Adam Kramer <adam@dwvd.nl>
2018-11-06 16:24:46 +0100

    Try new bash script

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -1,66 +1,33 @@
-#!/usr/bin/env bash
-# Provision WordPress Stable
 
-# fetch the first host as the primary domain. If none is available, generate a default using the site name
-DOMAIN=`get_primary_host "${VVV_SITE_NAME}".test`
-SITE_TITLE=`get_config_value 'site_title' "${DOMAIN}"`
-WP_VERSION=`get_config_value 'wp_version' 'latest'`
-WP_TYPE=`get_config_value 'wp_type' "single"`
-DB_NAME=`get_config_value 'db_name' "${VVV_SITE_NAME}"`
-DB_NAME=${DB_NAME//[\\\/\.\<\>\:\"\'\|\?\!\*-]/}
+project="project-name"
+
+echo "Commencing Bedrock Setup"
 
 # Make a database, if we don't already have one
-echo -e "\nCreating database '${DB_NAME}' (if it's not already there)"
-mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}"
-mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO wp@localhost IDENTIFIED BY 'wp';"
-echo -e "\n DB operations done.\n\n"
+echo "Creating database"
+mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS $project"
+mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON $project.* TO wp@localhost IDENTIFIED BY 'wp';"
 
-# Nginx Logs
-mkdir -p ${VVV_PATH_TO_SITE}/log
-touch ${VVV_PATH_TO_SITE}/log/error.log
-touch ${VVV_PATH_TO_SITE}/log/access.log
+# Download Bedrock
+if [ ! -d public_html ]
+then
 
-# Install and configure the latest stable version of WordPress
-if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/web/wp/wp-load.php" ]]; then
-    echo "Downloading Bedrock..."
-  eval cd .. && composer create-project roots/bedrock public_html
-  noroot wp core ${INSTALL_COMMAND} --url="${DOMAIN}" --quiet --title="${SITE_TITLE}" --admin_name=admin --admin_email="admin@local.test" --admin_password="password"
-	# noroot wp core download --version="${WP_VERSION}"
+  # Nginx Logs
+  echo "Creating logs"
+  mkdir -p ${VVV_PATH_TO_SITE}/log
+  touch ${VVV_PATH_TO_SITE}/log/error.log
+  touch ${VVV_PATH_TO_SITE}/log/access.log
+
+  echo "Installing Bedrock stack using Composer"
+  composer create-project roots/bedrock public_html
+
+  # Download Sage
+  # echo "Downloading Sage"
+  # cd public_html/web/app/themes
+  #   git clone https://github.com/roots/sage.git --branch 8.5.3 --single-branch
+  #   mv sage $project
 fi
 
-if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/web/wp/wp-config.php" ]]; then
-  echo "Configuring WordPress Stable..."
-  noroot wp db
-  noroot wp core config --dbname="${DB_NAME}" --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
-define( 'WP_DEBUG', true );
-define( 'SCRIPT_DEBUG', true );
-PHP
-fi
+# The Vagrant site setup script will restart Nginx for us
 
-# if ! $(noroot wp core is-installed); then
-#   echo "Installing WordPress Stable..."
-
-#   if [ "${WP_TYPE}" = "subdomain" ]; then
-#     INSTALL_COMMAND="multisite-install --subdomains"
-#   elif [ "${WP_TYPE}" = "subdirectory" ]; then
-#     INSTALL_COMMAND="multisite-install"
-#   else
-#     INSTALL_COMMAND="install"
-#   fi
-
-#   noroot wp core ${INSTALL_COMMAND} --url="${DOMAIN}" --quiet --title="${SITE_TITLE}" --admin_name=admin --admin_email="admin@local.test" --admin_password="password"
-# else
-#   echo "Updating WordPress Stable..."
-#   cd ${VVV_PATH_TO_SITE}/public_html
-#   noroot wp core update --version="${WP_VERSION}"
-# fi
-
-cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf.tmpl" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
-
-if [ -n "$(type -t is_utility_installed)" ] && [ "$(type -t is_utility_installed)" = function ] && `is_utility_installed core tls-ca`; then
-    sed -i "s#{{TLS_CERT}}#ssl_certificate /vagrant/certificates/${VVV_SITE_NAME}/dev.crt;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
-    sed -i "s#{{TLS_KEY}}#ssl_certificate_key /vagrant/certificates/${VVV_SITE_NAME}/dev.key;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
-else
-    sed -i "s#{{TLS_CERT}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
-    sed -i "s#{{TLS_KEY}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
-fi
+echo "$project Bedrock site now installed";
