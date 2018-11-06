5fa0d2f412db82d6c97bc8e848ffd34dc2eab38b
Adam Kramer <adam@dwvd.nl>
2018-11-06 16:54:34 +0100

    Add nginx config

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -18,8 +18,9 @@ then
   touch ${VVV_PATH_TO_SITE}/log/access.log
 
   echo "Installing Bedrock stack using Composer"
-  eval cd ..
-  composer create-project roots/bedrock public_html
+
+  # TODO: change eval to cd ${VVV_PATH_TO_SITE}/public_html or use mkdir command
+  eval cd .. && composer create-project roots/bedrock public_html
 
   # Download Sage
   # echo "Downloading Sage"
@@ -31,3 +32,13 @@ fi
 # The Vagrant site setup script will restart Nginx for us
 
 echo "$project Bedrock site now installed";
+
+cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf.tmpl" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+
+if [ -n "$(type -t is_utility_installed)" ] && [ "$(type -t is_utility_installed)" = function ] && `is_utility_installed core tls-ca`; then
+    sed -i "s#{{TLS_CERT}}#ssl_certificate /vagrant/certificates/${VVV_SITE_NAME}/dev.crt;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+    sed -i "s#{{TLS_KEY}}#ssl_certificate_key /vagrant/certificates/${VVV_SITE_NAME}/dev.key;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+else
+    sed -i "s#{{TLS_CERT}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+    sed -i "s#{{TLS_KEY}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+fi
